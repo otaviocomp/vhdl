7c53fdcb5187d4b7f4c0361277c83924fb576745
otaviocomp <otavio10espsanto@gmail.com>
2017-12-23 06:38:21 -0300

    mux

--- mux/mux.vhdl
+++ mux/mux.vhdl
@@ -6,16 +6,12 @@ entity mux is
 	(
 		sel : in std_logic_vector(1 downto 0);
 		a : in std_logic_vector(3 downto 0);
-		o : out std_logic;
+		o : out std_logic
 	);
 end mux;
 
 architecture hardware of mux is
 begin
-	a(0) <= '0';
-	a(1) <= '1';
-	a(2) <= '0';
-	a(3) <= '1';
 	with sel select
 		o <= a(0) when "00",
 			 a(1) when "01",
--- mux/mux_tb.vhdl
+++ mux/mux_tb.vhdl
@@ -14,16 +14,28 @@ architecture tb of mux_tb is
 		);
 	end component;	
 	signal insel : std_logic_vector(1 downto 0);
+	signal ina : std_logic_vector(3 downto 0);
+	signal outo : std_logic;
 begin
-	mapping : mux port map(sel => insel);
+	mapping : mux 
+	port map
+	(
+		sel => insel, 
+		a => ina, 
+		o => outo
+	);
 	process begin 
 		insel <= "00";
+		ina(0) <= '0';
 		wait for 15 ns;
 		insel <= "01";
+		ina(1) <= '1';
 		wait for 15 ns;
 		insel <= "10";
+		ina(2) <= '0';
 		wait for 15 ns;
 		insel <= "11";
+		ina(3) <= '1';
 		wait for 15 ns;
 		wait;
 	end process;
